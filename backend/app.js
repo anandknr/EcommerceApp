c85080b7a0d6e9763984d4592f6f7101c62ebbb8
anandknr <anand@LAPTOP-R4IJU881>
2023-01-19 12:35:13 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = new express();
+
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { Router } = require('express');
+const cors = require('cors');
+
+
+
+require('dotenv/config');
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+const api = process.env.API_URL;
+
+// const Product = require('./models/product');
+const productRouter = require('./routers/products');
+const categoryRouter = require('./routers/categories');
+const orderRouter = require('./routers/orders');
+const userRouter = require('./routers/users');
+
+app.use(api + "/products", productRouter);
+app.use(api + "/categories", categoryRouter);
+app.use(api + "/orders", orderRouter);
+app.use(api + "/users", userRouter);
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.CONNECTION_STRING, { dbName: 'eshop-db' })
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log("hello world" + api);
+});
