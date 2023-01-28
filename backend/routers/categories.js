c5a71238799e4b179c0c14628260f381cf0ba011
anandknr <anand@LAPTOP-R4IJU881>
2023-01-28 11:45:31 +0530

    29. Get Categories and Category Details

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -8,7 +8,16 @@ router.get(`/`, async(req, res) => {
     if (!categoryList) {
         res.status(500).json({ success: false })
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
+})
+
+router.get(`/:id`, async(req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+        res.status(500).json({ success: false, message: 'no category found' })
+    }
+    res.status(200).send(category);
 })
 
 router.post('/', async(req, res) => {
