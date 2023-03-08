3b3fb468bf1c9c481f0e557160d2771ad4f6598b
anandknr <anand@LAPTOP-R4IJU881>
2023-03-08 15:04:15 +0530

    42. Users Model & Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,63 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    email: {
+        type: String,
+        required: true
+    },
     image: String,
     countInStock: {
         type: Number,
         required: true
     }
-})
+});
+
+userSchema.virtual('id').get(function() {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+});
 
 exports.User = mongoose.model('User', userSchema);
